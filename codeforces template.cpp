d01130d4bc5ce75b41715050331832ec44ac88e9
yashgupta <yashg4824@gmail.com>
2024-10-04 11:32:03 +0530

    codeforces template added

---
+++ codeforces template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to solve the problem for a single test case
+void solveTestCase() {
+    // Read the input for the current test case
+    // Perform necessary operations and calculations
+
+    // Print the output for the current test case
+    // Make sure to follow the specified output format
+}
+
+int main() {
+    // Read the number of test cases
+    int t;
+    cin >> t;
+    // Iterate through each test case
+    for (int i = 0; i < t; i++) {
+        // Call the solveTestCase function to solve the problem for each test case
+        solveTestCase();
+    }
+}
+
